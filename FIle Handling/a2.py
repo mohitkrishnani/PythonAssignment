4972950234f977956bf2f0c9d66c11e1c3eb18a4
Mohit Krishnani <mohitkrishnani@gmail.com>
2021-08-01 02:53:59 +0530

    Changes in the file handling assignment

--- FIle Handling/a2.py
+++ FIle Handling/a2.py
@@ -1,24 +1,28 @@
-f=open("sample_1.csv","r")
-s = f.read()
-print(s)
-f.close()
-print()
-f=open("sample_1.csv","a")
-data = "\n4,four\n5,five"
-f.writelines(data)
-f.close()
-f=open("sample_1.csv","r")
-s= f.read()
-print(s)
-
-'''Output
-1,one
-2,two
-3,three
-
-1,one
-2,two
-3,three
-4,four
-5,five
+import csv
+rows = []
+with open("sample_1.csv",'r') as inp:
+	for row in csv.reader(inp):
+		print(row)
+		rows.append(row)
+	with open("sample_1.csv",'w',newline='') as out:
+		writer = csv.writer(out)
+		for row in rows:
+			row.append("Test")
+		print(rows)
+		for row in rows:
+			writer.writerow(row)	
+	for row in csv.reader(inp):
+		print(row)
 '''
+Output
+['1', 'one']
+['2', 'two']
+['3', 'three']
+['4', 'four']
+['5', 'five']
+
+['1', 'one', 'Test'], 
+['2', 'two', 'Test'], 
+['3', 'three', 'Test'], 
+['4', 'four', 'Test'], 
+['5', 'five', 'Test']'''
